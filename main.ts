5111bde6abc0f180ce9405af2c681f8275114f7e
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-29 12:01:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,54 @@
+// Task Datatypes
+
+//example no 1  (string)
+
+let favouriteCity : string= "Karachi"
+let favouriteCountry:string="Pakistan"
+console.log(`${favouriteCity}` + `${favouriteCountry}`)
+
+
+
+//example 2 (number)
+
+let a : number = 10
+let b : number = 25
+let c : number = 30
+console.log('a+b = ', a+b)
+console.log('a-b = ', a-b)
+console.log('a*c = ', a*c)
+console.log('c/a = ', c/a)
+
+
+
+
+
+// example 3(boolean)
+let isStudent : boolean = true
+let isTeacher : boolean = false
+console.log(isStudent)
+console.log(isTeacher)
+
+
+// example4(null)
+let friend :null= null
+console.log(friend)
+
+
+// example5 (undefined)
+let favouriteFood :undefined;
+console.log (favouriteFood)
+
+
+
+
+// example7 (bigint)
+let largeNum : bigint = 551536374866737747474998876543321190098777n
+console.log(largeNum)
+
+
+
+
+
+
+
+
